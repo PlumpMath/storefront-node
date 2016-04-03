0ca0f4613b5d7dd92dc6b26856e76abfe8340e37
Ramsey Nasser <ram@nas.sr>
2016-04-02 20:18:58 -0400

    clear server after timeout

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var app = express();
 var redis = require("redis"),
 client = redis.createClient(process.env.REDIS_URL);
 
+var sessionLength = 300;
+
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
@@ -14,7 +16,18 @@ client.on("error", function (err) {
 });
 
 app.post('/stroke', function (req, res) {
-  client.hset("strokes", req.body.id, req.body.points);
+  client.get("last-stroke", function(err, reply) {
+    var lastStroke = parseInt(reply);
+    if(Date.now() - lastStroke > sessionLength * 1000) {
+      client.del("strokes", function(err, reply) {
+        client.hset("strokes", req.body.id, req.body.points);
+        client.set("last-stroke", Date.now());
+      });
+    } else {
+      client.hset("strokes", req.body.id, req.body.points);
+      client.set("last-stroke", Date.now());
+    }
+  });
 });
 
 app.get('/data', function (req, res) {
